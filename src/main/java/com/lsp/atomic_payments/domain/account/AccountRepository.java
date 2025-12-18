7ae8e88677e4290a249b62f8a4a30ffd487e5467
lpato <lorenaspato@gmail.com>
2025-12-18 11:36:43 +0100

    feat: account repository interface

--- src/main/java/com/lsp/atomic_payments/domain/account/AccountRepository.java
+++ src/main/java/com/lsp/atomic_payments/domain/account/AccountRepository.java
@@ -1,5 +1,14 @@
 package com.lsp.atomic_payments.domain.account;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface AccountRepository {
+
+    Mono<Account> findById(AccountId accountId);
+
+    Mono<Account> save(Account account);
+
+    Flux<Account> findAll();
 
 }
