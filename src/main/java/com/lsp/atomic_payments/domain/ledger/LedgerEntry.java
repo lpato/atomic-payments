b36f5abca2ecbcf02dc1ea3234398e095dc3b24b
lpato <lorenaspato@gmail.com>
2025-12-18 11:28:07 +0100

    fix: use paymentId typed id

--- src/main/java/com/lsp/atomic_payments/domain/ledger/LedgerEntry.java
+++ src/main/java/com/lsp/atomic_payments/domain/ledger/LedgerEntry.java
@@ -1,17 +1,17 @@
 package com.lsp.atomic_payments.domain.ledger;
 
 import java.time.Instant;
-import java.util.UUID;
 
 import com.lsp.atomic_payments.domain.account.AccountId;
 import com.lsp.atomic_payments.domain.common.Money;
+import com.lsp.atomic_payments.domain.payment.PaymentId;
 
 public record LedgerEntry(
         LedgerEntryId ledgerEntryId,
         AccountId accountId,
+        PaymentId paymentId,
         Money amount,
         EntryType type,
-        UUID paymentId,
         Instant createdAt) {
 
 }
