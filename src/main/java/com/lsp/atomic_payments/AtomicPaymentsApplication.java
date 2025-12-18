fea46ba46a9bb085af19b349ffafc0a0235cea91
lpato <lorenaspato@gmail.com>
2025-12-18 09:12:55 +0100

    chore: bootstrap project structure

---
+++ src/main/java/com/lsp/atomic_payments/AtomicPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.lsp.atomic_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsp/atomic_payments/AtomicPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsp.atomic_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
