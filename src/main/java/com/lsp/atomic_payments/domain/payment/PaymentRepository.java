d6c48b9fa1e4c9b217c4882a438427984396a9fb
lpato <lorenaspato@gmail.com>
2025-12-18 11:37:14 +0100

    feat: payment repository interface

--- src/main/java/com/lsp/atomic_payments/domain/payment/PaymentRepository.java
+++ src/main/java/com/lsp/atomic_payments/domain/payment/PaymentRepository.java
@@ -1,5 +1,11 @@
 package com.lsp.atomic_payments.domain.payment;
 
+import reactor.core.publisher.Mono;
+
 public interface PaymentRepository {
+
+    Mono<Payment> findById(PaymentId paymentId);
+
+    Mono<Payment> save(Payment payment);
 
 }
