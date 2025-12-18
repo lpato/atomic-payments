8518a0c4a8299df588b14ee1a15a9ab3fa7f71b7
lpato <lorenaspato@gmail.com>
2025-12-18 11:37:01 +0100

    feat: ledger repository interface

--- src/main/java/com/lsp/atomic_payments/domain/ledger/LedgerRepository.java
+++ src/main/java/com/lsp/atomic_payments/domain/ledger/LedgerRepository.java
@@ -1,5 +1,20 @@
 package com.lsp.atomic_payments.domain.ledger;
 
+import java.util.List;
+
+import com.lsp.atomic_payments.domain.account.AccountId;
+import com.lsp.atomic_payments.domain.payment.PaymentId;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface LedgerRepository {
 
+    Mono<LedgerEntry> findById(LedgerEntryId ledgerEntryId);
+
+    Flux<LedgerEntry> findByAccountId(AccountId accountId);
+
+    Flux<LedgerEntry> findByPaymentId(PaymentId paymentId);
+
+    Flux<LedgerEntry> saveAll(List<LedgerEntry> entries);
 }
