be7eb1b270e40a5bbd1124334f4bd4c6e04c0db6
lpato <lorenaspato@gmail.com>
2025-12-26 07:58:10 +0100

    feat: add on error map translator for OptimisticLocking update consurrency issues

--- src/main/java/com/lsp/atomic_payments/domain/payment/PaymentService.java
+++ src/main/java/com/lsp/atomic_payments/domain/payment/PaymentService.java
@@ -3,12 +3,15 @@ package com.lsp.atomic_payments.domain.payment;
 import java.time.Instant;
 import java.util.List;
 
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.reactive.TransactionalOperator;
 
 import com.lsp.atomic_payments.domain.account.Account;
 import com.lsp.atomic_payments.domain.account.AccountRepository;
 import com.lsp.atomic_payments.domain.exception.AccountNotActiveException;
+import com.lsp.atomic_payments.domain.exception.ConcurrentAccountUpdateException;
 import com.lsp.atomic_payments.domain.exception.CurrencyMismatchException;
 import com.lsp.atomic_payments.domain.exception.InsufficientFundsException;
 import com.lsp.atomic_payments.domain.ledger.LedgerEntry;
@@ -16,6 +19,7 @@ import com.lsp.atomic_payments.domain.ledger.LedgerPair;
 import com.lsp.atomic_payments.domain.ledger.LedgerRepository;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -29,9 +33,12 @@ public class PaymentService {
 
     Mono<Payment> initiatePayment(PaymentCommand command) {
 
-        return transactionalOperator.execute(tx -> Mono.zip(
+        Mono<Payment> logic = Mono.zip(
+
                 accountRepository.findById(command.fromAccountId()),
-                accountRepository.findById(command.toAccountId())).flatMap(tuple -> {
+                accountRepository.findById(command.toAccountId()))
+
+                .flatMap(tuple -> {
                     Account fromAccount = tuple.getT1();
                     Account toAccount = tuple.getT2();
 
@@ -66,6 +73,24 @@ public class PaymentService {
                             .then(accountRepository.update(fromUpdated))
                             .then(accountRepository.update(toUpdated))
                             .thenReturn(payment);
-                })).single();
+                });
+
+        return logic
+                .as(transactionalOperator::transactional)
+                .onErrorMap(this::translateConcurrencyError);
+
     }
+
+    private Throwable translateConcurrencyError(Throwable ex) {
+        Throwable root = Exceptions.unwrap(ex);
+
+        if (root instanceof DuplicateKeyException ||
+                root instanceof OptimisticLockingFailureException) {
+
+            return new ConcurrentAccountUpdateException();
+        }
+
+        return ex;
+    }
+
 }
