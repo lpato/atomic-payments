0397f54dce3c54d7cca0f086dd9b7219b0f9f5b3
lpato <lorenaspato@gmail.com>
2025-12-30 12:03:08 +0100

    feat: method to get canonical payment request fro hash

--- src/main/java/com/lsp/atomic_payments/domain/payment/PaymentCommand.java
+++ src/main/java/com/lsp/atomic_payments/domain/payment/PaymentCommand.java
@@ -4,10 +4,16 @@ import com.lsp.atomic_payments.domain.account.AccountId;
 import com.lsp.atomic_payments.domain.common.Money;
 
 public record PaymentCommand(
-        AccountId fromAccountId,
-        AccountId toAccountId,
-        Money amount,
-        String reference,
-        String idempotencyKey // later
+                AccountId fromAccountId,
+                AccountId toAccountId,
+                Money amount,
+                String reference,
+                String idempotencyKey // later
 ) {
+        public String canonical() {
+                return fromAccountId().value() + "|" +
+                                toAccountId().value() + "|" +
+                                amount() + "|" +
+                                reference();
+        }
 }
